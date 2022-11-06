9b871cfe4e510692af07d215c03fe023563eb5a0
g2 Hong <80965583+g2da@users.noreply.github.com>
2022-11-06 12:23:08 +0900

    Delete 3월에 태어난 여성 회원 목록 출력하기.sql

--- SELECT/select/level1/3월에 태어난 여성 회원 목록 출력하기.sql
+++
@@ -1,3 +0,0 @@
-SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(date_of_birth, '%Y-%m-%d') as DATE_OF_BIRTH
-FROM member_profile
-WHERE date_of_birth like '%03%' and gender = 'W' and tlno is not null
